2a3901b1e63b5130811a0c2d535ac93611c5c723
root <root@template.(none)>
2016-07-12 16:23:23 +0800

    server get data

--- monitor-client.py
+++ monitor-client.py
@@ -6,6 +6,7 @@
 import sys
 import psutil
 import json
+import urllib2
 
 
 def _cpu(args, args_num):
@@ -20,8 +21,15 @@ def _cpu(args, args_num):
     return result
 
 
-def json_data(sysinfo_list):
-    return json.dumps(sysinfo_list)
+# def json_data(sysinfo_list):
+#     return json.dumps(sysinfo_list)
+
+
+def json_post(url, sysinfo_list, headers):
+    jdata = json.dumps(sysinfo_list)
+    req = urllib2.Request(url, jdata, headers = headers)
+    response = urllib2.urlopen(req)
+    return response.read()
 
 
 if __name__ == "__main__":
@@ -30,6 +38,13 @@ if __name__ == "__main__":
         args_num = args_len - 2
         args = [sys.argv[x] for x in range(2, args_len)]
         result = _cpu(args, args_num)
-        print type(json_data(result)), json_data(result)
+#         print type(json_data(result)), json_data(result)
+        headers = {
+    'Content-type': 'binary/octet-stream',
+#     'Content-length': len(postBody),
+    'Content-transfer-encoding': 'binary',
+}
+        url = "http://127.0.0.1:5000/"
+        json_post(url, result, headers)
     else:
         print "ERROR: too less arguments!"
